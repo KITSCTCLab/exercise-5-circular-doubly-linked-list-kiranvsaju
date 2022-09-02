495d978aeb024ecd2e4cd7506c1ee49ba8941cc9
kiranvsaju <91770235+kiranvsaju@users.noreply.github.com>
2022-09-02 08:03:51 +0530

    update 5

--- Main.py
+++ Main.py
@@ -16,7 +16,7 @@ class DoublyCircularLinkedList:
         if t is None:
             self.head = Node(data)
             self.count += 1
-            return True
+            
         else:
             while t.next is not self.head:
                 t = t.next
